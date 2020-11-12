d746ffbd29b82068ec5b583f58e35e5a9bf17bc0
ouafouaf <thebigwaf@gmail.com>
2020-11-12 17:34:28 +0800

    added fonts. beta

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ import math
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.colors import HexColor
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+
+# Register local fonts to report lab
+# rl_config.TTFSearchPath.append('/home/ouaf/Projects/bkwrm_page_generator/fonts')
+pdfmetrics.registerFont(TTFont('EnglishLing', 'EnglishLing-Regular.ttf'))
+# pdfmetrics.registerFont(TTFont('Jarman', 'Jarman.ttf'))
+pdfmetrics.registerFont(TTFont('Xumin', 'Xumin.ttf'))
 
 # TODO: 
 #   limit char/line in textarea https://codepen.io/chisenires/pen/eRWMZo (js)
@@ -20,7 +28,7 @@ app.config.from_object(__name__)
 ### WTForm ###
 class MyForm(FlaskForm):
     title = StringField('title')
-    lines = RadioField('lines', choices=[('6','6 （大）'),('8','8 （中）'),('10','10 （小）')])
+    lines = RadioField('lines', choices=[('6','6 （大）'),('8','8 （中）'),('10','10 （小）'),('12','12 （特小）')])
     text = TextAreaField('text')
 
 ### Create pdf function ###
@@ -30,17 +38,18 @@ def create_pdf(my_canvas, title, lines_per_page, text):
     if number_of_pages == 0:
         number_of_pages = 1
     line_spacing  = (A4[1]*0.95) / (int(lines_per_page) +1) / 5
-    fontsize = 315 / int(lines_per_page)
+    fontsize = 300 / int(lines_per_page)
 
     for p in range(1, number_of_pages+1):
         ### Write title and page number
-        my_canvas.setFont('Times-Italic', fontsize)
+        # my_canvas.setFont('Times-Italic', fontsize)
+        my_canvas.setFont('Times-Roman', fontsize)
         page_tag = f"({p}/{number_of_pages})"
         txt = f"{title} {page_tag}"
         my_canvas.drawString(40, A4[1]-50, txt)
 
         ### Write logo
-        my_canvas.drawImage('bookworm.jpg', 500, 700, width=70,
+        my_canvas.drawImage('bookworm.jpg', 500, 700, width=90,
                  preserveAspectRatio=True, mask='auto')
 
 
@@ -48,7 +57,7 @@ def create_pdf(my_canvas, title, lines_per_page, text):
         # initialize x and y
         x1 = 40
         x2 = 555
-        y = A4[1] - (line_spacing * 5)
+        y = A4[1] - (line_spacing * 6)
 
         for l in range(int(lines_per_page)):
             my_canvas.setLineWidth(.4)
@@ -64,15 +73,19 @@ def create_pdf(my_canvas, title, lines_per_page, text):
 
         ### Write text for that page
         # initialize x and y
-        y = A4[1] - (line_spacing * 7)
+        # y = A4[1] - (line_spacing * 8)
+        y = A4[1] - (line_spacing * 9) + 1  # for EnglishLing
 
         # fetch lines index
-        first_line_idx = (p-1)*(int(lines_per_page))+1
-        last_line_idx = (p-1)*(int(lines_per_page))+int(lines_per_page)+1
+        first_line_idx = (p-1)*(int(lines_per_page))
+        last_line_idx = (p-1)*(int(lines_per_page))+int(lines_per_page)
+        # fontsize2 = 350 / int(lines_per_page)
+        fontsize2 = 460 / int(lines_per_page)     # For EnglishLing
 
         for l in lines[first_line_idx:last_line_idx]:
-            my_canvas.setFont('Times-Roman', fontsize)
-            my_canvas.setFillColor(HexColor(0xdddddd))
+            # my_canvas.setFont('Xumin', fontsize2)
+            my_canvas.setFont('EnglishLing', fontsize2)
+            # my_canvas.setFillColor(HexColor(0xdddddd))
             my_canvas.drawString(44, y, l)
             y -= (line_spacing * 5)
 
